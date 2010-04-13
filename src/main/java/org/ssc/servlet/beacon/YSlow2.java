464808523f4580ca3a4f3f74bf51f9cf5ea0b1c2
John Reyes <johnlreyes@yahoo.com>
2010-04-13 12:40:39 +0800

    updated

--- src/main/java/org/ssc/servlet/beacon/YSlow2.java
+++ src/main/java/org/ssc/servlet/beacon/YSlow2.java
@@ -13,17 +13,18 @@ public class YSlow2 extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		System.out.println("["+getClass().getName()+":doGet] - START");
-		String url = ((String[])req.getParameterMap().get("u"))[0];
-		System.out.println("url="+url);
-		DBUtil.addUrl(url);
-		System.out.println("["+getClass().getName()+":doGet] - END");
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("[" + getClass().getName() + ":doGet] - START");
+		String[] uArr = (String[]) req.getParameterMap().get("u");
+		if (uArr != null && uArr.length > 1) {
+			String url = uArr[0];
+			System.out.println("url=" + url);
+			DBUtil.addUrl(url);
+		}
+		System.out.println("[" + getClass().getName() + ":doGet] - END");
 	}
 
-	public void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		doGet(req, resp);
 	}
 }
